e251983d8e3c0106678309060ac5e7c07eb6003f
Glasses321 <Laura_broekstra@hotmail.com>
2018-04-05 11:42:30 +0200

    Update simpleForm.js

--- FormSetUp/src/js/viewModels/simpleForm.js
+++ FormSetUp/src/js/viewModels/simpleForm.js
@@ -8,15 +8,14 @@ define(['ojs/ojcore', 'knockout', 'jquery', 'ojs/ojinputtext', 'ojs/ojlabel', 'o
       self.courseName = ko.observable();
       self.courseDate = ko.observable();
       self.courseInstructor = ko.observable();
-      self.instructors = ko.observable();
       self.studentName = ko.observable();
       self.studentGrade = ko.observable();
       self.courseRemarks = ko.observable();
       self.courseRating = ko.observable();
 
       self.instructors = [
-        {value: 'JR', label: 'Jeroen Rijnboutt'},
-        {value: 'LB', label: 'Laura Broekstra'}
+        {value: 'Jeroen Rijnboutt', label: 'Jeroen Rijnboutt'},
+        {value: 'Laura Broekstra', label: 'Laura Broekstra'}
       ];
 
       self.submitForm = function() {

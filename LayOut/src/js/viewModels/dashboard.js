6355327443db9dc923de769d92c481f210bc9817
Glasses321 <Laura_broekstra@hotmail.com>
2018-04-10 14:02:27 +0200

    Add files via upload

---
+++ LayOut/src/js/viewModels/dashboard.js
@@ -0,0 +1,9 @@
+define(['ojs/ojcore', 'knockout', 'jquery'],
+ function(oj, ko, $) {
+  
+    function DashboardViewModel() {
+      var self = this;
+    }
+    return new DashboardViewModel();
+  }
+);

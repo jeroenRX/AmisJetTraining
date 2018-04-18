d53a0d5191160fe244317faa0bc65025725ed0af
Jeroen Rijnboutt <jeroen.rijnboutt@amis.nl>
2018-04-18 14:14:48 +0200

    JET 5 version for Knockout application

---
+++ Knockout5/src/js/viewModels/evaluation.js
@@ -0,0 +1,17 @@
+/**
+ * @license
+ * Copyright (c) 2014, 2018, Oracle and/or its affiliates.
+ * The Universal Permissive License (UPL), Version 1.0
+ */
+/*
+ * Your incidents ViewModel code goes here
+ */
+define(['ojs/ojcore', 'knockout', 'jquery'],
+ function(oj, ko, $) {
+  
+    function EvaluationViewModel() {
+      var self = this;
+    
+    return new EvaluationViewModel();
+  }
+);
